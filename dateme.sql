ac23ac2bd9874e9b47bbe03571c786587952fff1
realchief <slobodan.topdev@gmail.com>
2018-10-04 12:32:32 +0200

    update

--- dateme.sql
+++ dateme.sql
@@ -251,7 +251,7 @@ CREATE TABLE `czars` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `last_login` datetime DEFAULT NULL,
-  `last_ip` varchar(32) NOT NULL,
+  `last_ip` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -866,4 +866,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-04 12:24:42
+-- Dump completed on 2018-10-04 12:32:19
